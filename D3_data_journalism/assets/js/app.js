985b22bc60a0995043a1c41a46e9b02b2178ce6a
Chad Milburn <cmilburn@ballstate.bsu.edu>
2021-04-10 15:35:17 -0400

    working on initial chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,20 @@
 // @TODO: YOUR CODE HERE!
+// create axis variable
+var selectedXAxis = "poverty";
+var selectedYAxis = "healthcare";
+// function to update xscale
+function xScale(data, selectedXAxis, chartWidth) {
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[selectedXAxis])*.8,
+        d3.max(data, d => d[selectedXAxis]) * 1.1])
+        .range([0, chartWidth]);
+    return xLinearScale;
+}
+// function to update xaxis
+function renderXAxes(newXScale, xAxis){
+    var bottomAxis = d3.axisBottom(newXScale);
+    xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+    return xAxis;
+}
